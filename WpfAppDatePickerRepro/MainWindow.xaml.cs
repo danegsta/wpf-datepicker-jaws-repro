035ca3bb4c61562030e07748bbb1717c1e6c137d
David Negstad <David.Negstad@microsoft.com>
2022-07-06 09:54:13 -0700

    Add repro app

---
+++ WpfAppDatePickerRepro/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfAppDatePickerRepro
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            this.DataContext = new MainWindowViewModel();
+
+            // Handle the GotFocus event on AccountExpirationDatePicker. Simply adding event handler doesn't work when user uses mouse to select the datepicker text box.
+            // This is the workaround.
+            AccountExpirationDatePicker.AddHandler(DatePicker.GotFocusEvent, new RoutedEventHandler(AccountExpirationDatePicker_GotFocus), true);
+        }
+
+        private void AccountExpirationDatePicker_GotFocus(object sender, RoutedEventArgs e)
+        {
+            e.Handled = false;
+        }
+    }
+}
---
+++ WpfAppDatePickerRepro/MainWindowViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WpfAppDatePickerRepro
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        private DateTime? _accountExpirationDate = DateTime.UtcNow;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTime? AccountExpirationDate
+        {
+            get
+            {
+                return _accountExpirationDate;
+            }
+
+            set
+            {
+                if (_accountExpirationDate != value)
+                {
+                    _accountExpirationDate = value;
+                    OnPropertyChanged(nameof(AccountExpirationDate));
+                }
+            }
+        }
+
+        // Create the OnPropertyChanged method to raise the event
+        // The calling member's name will be used as the parameter.
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
